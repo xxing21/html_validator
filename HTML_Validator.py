07414d53fabf4ccd06485d871d368ed978beb980
xxing21 <60284417+xxing21@users.noreply.github.com>
2020-02-14 18:16:03 -0800

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -20,6 +20,8 @@ def validate_html(html):
     validate = True
     tags =_extract_tags(html)
     s = []
+    if len(tags) == 0:
+        return True
     for i in range(len(tags)):
         if "/" not in tags[i]:
             s.append(tags[i])

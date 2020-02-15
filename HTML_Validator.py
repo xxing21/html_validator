f09858392a49e4ecb45608f7d5279f129c270847
xxing21 <60284417+xxing21@users.noreply.github.com>
2020-02-15 15:56:46 -0800

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -52,12 +52,10 @@ def _extract_tags(html):
     import re
     
     tags = re.findall(r'<[^>]+>', html)
-    return tags
 
-# extra credit:
-#    if len(tags) != 0:
-#       return tags
-#   else:
-#       raise ValueError("found < without matching >")
+    if len(tags) != 0:
+       return tags
+    else:
+       raise ValueError("found < without matching >")
 
 
